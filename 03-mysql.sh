8baad5e4dba4f4ad9f7195015e973184c39865be
snraju98 <nagarajusagiraju123@gmail.com>
2026-05-26 19:10:32 +0530

    api doc added

--- 03-mysql.sh
+++ 03-mysql.sh
@@ -27,16 +27,13 @@ else
 fi
 
 }
-#redis
 
-dnf module disable redis -y  &>> $LOGS_FILE
-dnf module enable redis:7 -y &>> $LOGS_FILE
-dnf install redis -y  &>> $LOGS_FILE
-VALIDATE $? "Installing Redis:7"
+dnf install mysql-server -y &>> $LOGS_FILE
+VALIDATE $? "Installing MySQL Server"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no'  /etc/redis/redis.conf
-VALIDATE $? "Allowing remote connections"
+systemctl enable mysqld &>> $LOGS_FILE
+systemctl start mysqld  &>> $LOGS_FILE
+VALIDATE $? "Enable amd start MySQL Server"
 
-systemctl enable redis &>> $LOGS_FILE
-systemctl start redis &>> $LOGS_FILE
-VALIDATE $? "Started Redis"
+mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATE $? "Setting up root password"
