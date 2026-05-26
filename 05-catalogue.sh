a555e5bfa90198d155ba151eb34f7869f54cf111
snraju98 <nagarajusagiraju123@gmail.com>
2026-05-26 19:43:45 +0530

    api doc added

---
+++ 05-catalogue.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/roboshop"
+sudo mkdir -p / $LOGS_FOLDER
+sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
+sudo chmod -R 755 $LOGS_FOLDER
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+   echo -e "$TIMESTAMP [ERROR] $R Please run the script with root access $N" | tee -a $LOGS_FILE
+   exit 1
+fi
+
+VALIDATE(){
+ if [ $1 -ne 0 ]; then
+  echo -e "$TIMESTAMP [ERROR} $2 .. $R FAILURE $N"  | tee -a $LOGS_FILE
+  exit 1
+else
+  echo -e "$TIMESTAMP [INFO] $2 .. $G  SUCCESS $N" | tee -a $LOGS_FILE
+fi
+
+}
+
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+VALIDATE $? "Installing NodeJS:20"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+VALIDATE $? "Creating roboshop system user"
+
+mkdir /app
+VALIDATE $? "Creating app dirrectory"
